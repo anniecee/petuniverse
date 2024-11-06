1b5e4def181249fa9c2cc8c72c1dde78c24c6643
Annie <atc16@sfu.ca>
2024-11-06 10:13:26 -0800

    added photo for each listing

--- app/src/main/java/com/example/pet_universe/ui/sellerView/SellerListingsAdapter.kt
+++ app/src/main/java/com/example/pet_universe/ui/sellerView/SellerListingsAdapter.kt
@@ -1,7 +1,9 @@
 package com.example.pet_universe.ui.sellerView
 
+import android.graphics.BitmapFactory
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.pet_universe.R
@@ -11,10 +13,17 @@ class SellerListingsAdapter(private val sellerListings : MutableList<Listing>) :
     RecyclerView.Adapter<SellerListingsAdapter.ViewHolder>() {
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        // TODO: Add more fields as needed
         val listingTitle: TextView = itemView.findViewById(R.id.listingTitle)
         val listingPrice: TextView = itemView.findViewById(R.id.listingPrice)
         val listingDescription: TextView = itemView.findViewById(R.id.listingDescription)
+        val listingCategory: TextView = itemView.findViewById(R.id.listingCategory)
+        val listingPhoto: ImageView = itemView.findViewById(R.id.listingPhoto)
+
+        // This function is called to set the image of the listing
+        fun bindData(listing: Listing) {
+            val bitmap = BitmapFactory.decodeByteArray(listing.photo, 0, listing.photo.size)
+            listingPhoto.setImageBitmap(bitmap)
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -28,10 +37,13 @@ class SellerListingsAdapter(private val sellerListings : MutableList<Listing>) :
 
     // This function is called when the RecyclerView needs to display data at a certain position
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        // TODO: Add more fields as needed
         holder.listingTitle.text = sellerListings[position].title
         holder.listingPrice.text = "$" + sellerListings[position].price.toString()
         holder.listingDescription.text = sellerListings[position].description
+        holder.listingCategory.text = "Category: " + sellerListings[position].category
+
+        // Set image of listing
+        holder.bindData(sellerListings[position])
     }
 
 }
